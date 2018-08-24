d309194be1b0e0b9c848884cf151694123812e0e
Gimenez, Aleix <aleix.gimenez@sky.uk>
2018-08-24 16:00:29 +0100

    using builders in tests

--- src/test/kotlin/com/github/unqualsevol/marsroverkata/ReceivingTurningCommandTest.kt
+++ src/test/kotlin/com/github/unqualsevol/marsroverkata/ReceivingTurningCommandTest.kt
@@ -1,4 +1,5 @@
 package com.github.unqualsevol.marsroverkata
+import com.github.unqualsevol.marsroverkata.helpers.RoverBuilder
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -6,88 +7,88 @@ class ReceivingTurningCommandTest {
     @Test
     fun should_point_east_when_facing_north_and_turns_right() {
         //Given
-        val startRover = Rover(2, 2, "N")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("N").build()
         //When
         startRover.sendCommands("r")
         //Then
-        val expectedRover = Rover(2, 2, "E")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("E").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_point_south_when_facing_east_and_turns_right() {
         //Given
-        val startRover = Rover(2, 2, "E")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("E").build()
         //When
         startRover.sendCommands("r")
         //Then
-        val expectedRover = Rover(2, 2, "S")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("S").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_point_west_when_facing_south_and_turns_right() {
         //Given
-        val startRover = Rover(2, 2, "S")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("S").build()
         //When
         startRover.sendCommands("r")
         //Then
-        val expectedRover = Rover(2, 2, "W")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("W").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_point_north_when_facing_west_and_turns_right() {
         //Given
-        val startRover = Rover(2, 2, "W")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("W").build()
         //When
         startRover.sendCommands("r")
         //Then
-        val expectedRover = Rover(2, 2, "N")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("N").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_point_west_when_facing_north_and_turns_left() {
         //Given
-        val startRover = Rover(2, 2, "N")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("N").build()
         //When
         startRover.sendCommands("l")
         //Then
-        val expectedRover = Rover(2, 2, "W")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("W").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_point_north_when_facing_east_and_turns_left() {
         //Given
-        val startRover = Rover(2, 2, "E")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("E").build()
         //When
         startRover.sendCommands("l")
         //Then
-        val expectedRover = Rover(2, 2, "N")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("N").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_point_east_when_facing_south_and_turns_left() {
         //Given
-        val startRover = Rover(2, 2, "S")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("S").build()
         //When
         startRover.sendCommands("l")
         //Then
-        val expectedRover = Rover(2, 2, "E")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("E").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_point_south_when_facing_west_and_turns_left() {
         //Given
-        val startRover = Rover(2, 2, "W")
+        val startRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("W").build()
         //When
         startRover.sendCommands("l")
         //Then
-        val expectedRover = Rover(2, 2, "S")
+        val expectedRover = RoverBuilder.aMarsRoverAtIrrelevantPosition().pointing("S").build()
         assertEquals(expectedRover, startRover)
     }
 }
