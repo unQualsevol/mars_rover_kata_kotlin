173bd005e358793d2330da26139c16b0d367f31d
Gimenez, Aleix <aleix.gimenez@sky.uk>
2018-08-31 10:25:40 +0100

    using builders in tests

--- src/test/kotlin/com/github/unqualsevol/marsroverkata/ReceivingDisplacementCommandTest.kt
+++ src/test/kotlin/com/github/unqualsevol/marsroverkata/ReceivingDisplacementCommandTest.kt
@@ -1,4 +1,5 @@
 package com.github.unqualsevol.marsroverkata
+import com.github.unqualsevol.marsroverkata.helpers.RoverBuilder
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -7,88 +8,88 @@ class ReceivingDisplacementCommandTest {
     @Test
     fun should_increase_Y_when_facing_north_and_moves_forward() {
         //Given
-        val startRover = Rover(2, 2, "N")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("N").build()
         //When
         startRover.sendCommands("f")
         //Then
-        val expectedRover = Rover(2, 3, "N")
+        val expectedRover = RoverBuilder.aMarsRover().at(2, 3).pointing("N").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_increase_X_when_facing_east_and_moves_forward() {
         //Given
-        val startRover = Rover(2, 2, "E")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("E").build()
         //When
         startRover.sendCommands("f")
         //Then
-        val expectedRover = Rover(3, 2, "E")
+        val expectedRover = RoverBuilder.aMarsRover().at(3, 2).pointing("E").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_decrease_Y_when_facing_south_and_moves_forward() {
         //Given
-        val startRover = Rover(2, 2, "S")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("S").build()
         //When
         startRover.sendCommands("f")
         //Then
-        val expectedRover = Rover(2, 1, "S")
+        val expectedRover = RoverBuilder.aMarsRover().at(2, 1).pointing("S").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_decrease_X_when_facing_west_and_moves_forward() {
         //Given
-        val startRover = Rover(2, 2, "W")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("W").build()
         //When
         startRover.sendCommands("f")
         //Then
-        val expectedRover = Rover(1, 2, "W")
+        val expectedRover = RoverBuilder.aMarsRover().at(1, 2).pointing("W").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_decrease_Y_when_facing_north_and_moves_backward() {
         //Given
-        val startRover = Rover(2, 2, "N")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("N").build()
         //When
         startRover.sendCommands("b")
         //Then
-        val expectedRover = Rover(2, 1, "N")
+        val expectedRover = RoverBuilder.aMarsRover().at(2, 1).pointing("N").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_decrease_X_when_facing_east_and_moves_backward() {
         //Given
-        val startRover = Rover(2, 2, "E")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("E").build()
         //When
         startRover.sendCommands("b")
         //Then
-        val expectedRover = Rover(1, 2, "E")
+        val expectedRover = RoverBuilder.aMarsRover().at(1, 2).pointing("E").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_increase_Y_when_facing_south_and_moves_backward() {
         //Given
-        val startRover = Rover(2, 2, "S")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("S").build()
         //When
         startRover.sendCommands("b")
         //Then
-        val expectedRover = Rover(2, 3, "S")
+        val expectedRover = RoverBuilder.aMarsRover().at(2, 3).pointing("S").build()
         assertEquals(expectedRover, startRover)
     }
 
     @Test
     fun should_increase_X_when_facing_west_and_moves_backward() {
         //Given
-        val startRover = Rover(2, 2, "W")
+        val startRover = RoverBuilder.aMarsRover().at(2, 2).pointing("W").build()
         //When
         startRover.sendCommands("b")
         //Then
-        val expectedRover = Rover(3, 2, "W")
+        val expectedRover = RoverBuilder.aMarsRover().at(3, 2).pointing("W").build()
         assertEquals(expectedRover, startRover)
     }
 }
